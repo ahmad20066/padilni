e2827921e58da9f036035fb33c7b5ae8dab9ebb0
ahmad20066 <ahmadafif613@gmail.com>
2023-09-29 07:10:31 -0400

    aa

---
+++ lib/presentation/search/controllers/search_controller.dart
@@ -0,0 +1,43 @@
+import 'package:get/get.dart';
+import 'package:padilni/models/product/product_model.dart';
+
+class SearchScreenController extends GetxController {
+  List<ProductModel> products = [
+    ProductModel(
+        id: 1,
+        name: "Market",
+        image:
+            "https://www.collinsdictionary.com/images/full/market_large_354703739_1000.jpg",
+        type: "Food"),
+    ProductModel(
+        id: 1,
+        name: "Box",
+        image:
+            "https://www.tinyboxcompany.co.uk/media/catalog/product/F/M/FMKRCU_22.jpg?width=800&height=800&store=default&image-type=image",
+        type: "Stuff"),
+    ProductModel(
+        id: 1,
+        name: "Box",
+        image:
+            "https://www.tinyboxcompany.co.uk/media/catalog/product/F/M/FMKRCU_22.jpg?width=800&height=800&store=default&image-type=image",
+        type: "Stuff"),
+    ProductModel(
+        id: 1,
+        name: "Box",
+        image:
+            "https://www.tinyboxcompany.co.uk/media/catalog/product/F/M/FMKRCU_22.jpg?width=800&height=800&store=default&image-type=image",
+        type: "Stuff"),
+    ProductModel(
+        id: 1,
+        name: "Box",
+        image:
+            "https://www.tinyboxcompany.co.uk/media/catalog/product/F/M/FMKRCU_22.jpg?width=800&height=800&store=default&image-type=image",
+        type: "Stuff"),
+    ProductModel(
+        id: 1,
+        name: "Box",
+        image:
+            "https://www.tinyboxcompany.co.uk/media/catalog/product/F/M/FMKRCU_22.jpg?width=800&height=800&store=default&image-type=image",
+        type: "Stuff"),
+  ];
+}
--- lib/presentation/search/screens/search_screen.dart
+++ lib/presentation/search/screens/search_screen.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:padilni/presentation/home/widgets/list/list_item.dart';
+import 'package:padilni/presentation/search/controllers/search_controller.dart';
 import 'package:padilni/utils/widgets/auth_appbar.dart';
+import 'package:padilni/utils/widgets/body_container.dart';
 import 'package:padilni/utils/widgets/home_appbar.dart';
 
 class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
-
+  SearchScreen({super.key});
+  final SearchScreenController controller = Get.put(SearchScreenController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: HomeAppbar(text: "Search"),
-      body: Container(),
+      body: Stack(
+        children: [
+          HomeAppbar(text: "Search"),
+          BodyContainer(
+            initialSize: 0.8,
+            maxSize: 0.85,
+            minSize: 0.8,
+            children: [
+              GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: controller.products.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, childAspectRatio: 0.9),
+                  itemBuilder: (context, index) {
+                    return ListItem(product: controller.products[index]);
+                  }),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
